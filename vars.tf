f3d684e481d8cb9625544ff1642358cbc3e773ee
Sivakumar S <sivakumarit42@gmail.com>
2023-04-04 09:20:11 +0530

    changes

--- vars.tf
+++ vars.tf
@@ -5,11 +5,11 @@ variable "folders" {
 variable "jobs" {
   default = [
     { name = "roboshop", folder = "Infra", repo_url = "https://github.com/sivakumarit42/roboshop-infra" },
-    { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/sivakumarit42/roboshop-infra" },
-    { name = "cart", folder = "CI-Pipeline", repo_url = "https://github.com/sivakumarit42/roboshop-infra" },
-    { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/sivakumarit42/roboshop-infra" },
-    { name = "user", folder = "CI-Pipeline", repo_url = "https://github.com/sivakumarit42/roboshop-infra" },
-    { name = "shipping", folder = "CI-Pipeline", repo_url = "https://github.com/sivakumarit42/roboshop-infra" },
-    { name = "payment", folder = "CI-Pipeline", repo_url = "https://github.com/sivakumarit42/roboshop-infra" }
+    { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/sivakumarit42/frontend" },
+    { name = "cart", folder = "CI-Pipeline", repo_url = "https://github.com/sivakumarit42/cart" },
+    { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/sivakumarit42/catalogue" },
+    { name = "user", folder = "CI-Pipeline", repo_url = "https://github.com/sivakumarit42/user" },
+    { name = "shipping", folder = "CI-Pipeline", repo_url = "https://github.com/sivakumarit42/shipping" },
+    { name = "payment", folder = "CI-Pipeline", repo_url = "https://github.com/sivakumarit42/payment" }
   ]
 }
