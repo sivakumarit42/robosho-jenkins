b2d460b94a612a75b43afc35653ea79286e8d506
Sivakumar S <sivakumarit42@gmail.com>
2023-04-06 05:23:33 +0530

    changes

--- main.tf
+++ main.tf
@@ -19,3 +19,14 @@ resource "jenkins_job" "job" {
     ignore_changes = [template]
   }
 }
+data "aws_instance" "jenkins" {
+  instance_id = "i-0f4190856737ed64d"
+}
+
+resource "aws_route53_record" "jenkins" {
+  zone_id = "Z04370391Z7FE49YGIYQ7"
+  name    = "jenkins.devopsb72.online"
+  type    = "A"
+  ttl     = 30
+  records = [data.aws_instance.jenkins.public_ip]
+}
