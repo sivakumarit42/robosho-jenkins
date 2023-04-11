64948ac4bc9137f2a99d18949b4045013a7819e8
Sivakumar S <sivakumarit42@gmail.com>
2023-04-11 17:07:08 +0530

    changes

--- vars.tf
+++ vars.tf
@@ -2,9 +2,8 @@ variable "folders" {
   default = ["Infra", "CI-Pipeline"]
 }
 
-variable "jobs" {
+variable "m-jobs" {
   default = [
-    { name = "roboshop", folder = "Infra", repo_url = "https://github.com/sivakumarit42/roboshop-infra" },
     { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/sivakumarit42/frontend" },
     { name = "cart", folder = "CI-Pipeline", repo_url = "https://github.com/sivakumarit42/cart" },
     { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/sivakumarit42/catalogue" },
@@ -12,4 +11,13 @@ variable "jobs" {
     { name = "shipping", folder = "CI-Pipeline", repo_url = "https://github.com/sivakumarit42/shipping" },
     { name = "payment", folder = "CI-Pipeline", repo_url = "https://github.com/sivakumarit42/payment" }
   ]
+}
+
+variable "s-jobs" {
+  default = [
+    { name = "roboshop", folder = "Infra", repo_url = "https://github.com/sivakumarit42/roboshop-infra" }
+  ]
+}
+variable "force" {
+  default = false
 }
